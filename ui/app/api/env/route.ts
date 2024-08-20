a1195613acf4357b7254ee0c8f9662940aaeda50
Andrew Pennington <email@andrew-pennington.dev>
2024-08-20 22:28:56 +0100

    Added a fallback to statically compiled environment variables if live ones are not defined

--- ui/app/api/env/route.ts
+++ ui/app/api/env/route.ts
@@ -7,8 +7,8 @@ export const runtime = "edge"
 export async function GET(_request: Request) {
   // Access environment variables
   const envVars = {
-    'BACKEND_API_URL': process.env.BACKEND_API_URL,
-    'BACKEND_WS_URL': process.env.BACKEND_WS_URL
+    'BACKEND_API_URL': process.env.BACKEND_API_URL ?? process.env.NEXT_PUBLIC_API_URL,
+    'BACKEND_WS_URL': process.env.BACKEND_WS_URL ?? process.env.NEXT_PUBLIC_WS_URL
   }
 
   // Return the environment variables as a JSON response
