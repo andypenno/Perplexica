8f5b1956ed5f059369eab838e7bd69c4522267d0
Andrew Pennington <email@andrew-pennington.dev>
2024-08-23 23:20:42 +0100

    Oops

--- ui/lib/serverEnvironment.ts
+++ ui/lib/serverEnvironment.ts
@@ -6,7 +6,7 @@ export async function getServerEnv(envVar: string): Promise<string> {
   let result: string | undefined;
   switch (envVar) {
       case "BACKEND_API_URL":
-          result = process.env.BACKEND_API_URL ?? process.env.NEXT_PUBLIC_API_URL;4
+          result = process.env.BACKEND_API_URL ?? process.env.NEXT_PUBLIC_API_URL;
           break;
       case "BACKEND_WS_URL":
           result = process.env.BACKEND_WS_URL ?? process.env.NEXT_PUBLIC_WS_URL;
